95dc2f4fffc307078bf542b99a8cbe637a937408
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-10-29 09:49:26 +0200

    Add Jest configuration to ignore @fullcalendar transform. Jest test is working with @fullcalendar.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Appointments from './components/Appointments'
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Appointments />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
